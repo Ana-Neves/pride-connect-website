abc7a5bdcb2d147c5a2088fe365e2e34bc7bfb5f
Ana-Neves <ana.carolinemms22@gmail.com>
2024-09-15 16:57:55 -0300

    sending pictures and app.js upgrade

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import MissionSection from './components/MissionSection';
+import OfferingsSection from './components/OfferingsSection';
+import RightsSection from './components/RightsSection';
+import MentorshipSection from './components/MentorshipSection';
+import JoinUsSection from './components/JoinUsSection';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <MissionSection />
+      <OfferingsSection />
+      <RightsSection />
+      <MentorshipSection />
+      <JoinUsSection />
     </div>
   );
 }
